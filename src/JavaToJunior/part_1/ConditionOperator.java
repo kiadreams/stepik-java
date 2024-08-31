66b47285bbd4afddd4785340c5199845c38255b9
Igor Kobelev <kia1809@yandex.ru>
2024-08-31 21:01:28 +0300

    section 2.3 has been completed

---
+++ src/JavaToJunior/part_1/ConditionOperator.java
@@ -0,0 +1,15 @@
+package JavaToJunior.part_1;
+
+public class ConditionOperator {
+
+    public static void main(String[] args) {
+        int temp = 23;
+        if (temp > 25) {
+            System.out.println("Кондиционер включён!");
+        } else if (temp < 20) {
+            System.out.println("Кондиционер выключен!");
+        } else {
+            System.out.println("Температура в норме!!!");
+        }
+    }
+}
