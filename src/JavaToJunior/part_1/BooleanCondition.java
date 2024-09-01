87b99b3251585a4e7ce2931a6219b967d31643aa
Igor Kobelev <kia1809@yandex.ru>
2024-09-01 17:17:06 +0300

    section 2.6 has been completed

---
+++ src/JavaToJunior/part_1/BooleanCondition.java
@@ -0,0 +1,26 @@
+package JavaToJunior.part_1;
+
+public class BooleanCondition {
+
+    public static void main(String[] args) {
+        int temp = 25;
+        int time = 21;
+
+        boolean sampleTrue = true;
+        boolean sampleFalse = false;
+        System.out.println(sampleFalse);
+        System.out.println(sampleTrue);
+
+        boolean hot = temp >= 25;
+        boolean cold = temp <= 20;
+        boolean isNight = time > 22 || time < 6;
+
+        if (hot && !isNight) {
+            System.out.println("Кондиционер включён!");
+        } else if (cold) {
+            System.out.println("Кондиционер выключен!");
+        } else {
+            System.out.println("Температура в норме!!!");
+        }
+    }
+}
