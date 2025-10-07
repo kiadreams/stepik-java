15d808660d81d4f3a691a0a4fa5cfb31f882d7cc
Igor Kobelev <kia1809@yandex.ru>
2025-10-07 22:34:58 +0300

    change one file

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,6 +1,6 @@
 package calculator;
 
-public class SomeMain {
+public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("10 + 5 = " + calculator.add(10, 5));
